09447447f5298cf7519aa9f7da8c0c07c38b37dc
mendesmcmg <claraplanejamento@gmail.com>
2024-10-04 11:58:56 -0300

    🌈 day 1 - ex. 1 (refactored)

--- hackerrank/1-week-kit/day1-plus-minus.py
+++ hackerrank/1-week-kit/day1-plus-minus.py
@@ -13,25 +13,23 @@ import sys
 #
 
 def plusMinus(arr):
-    negs, zero, pos = 0, 0, 0
-    for number in arr:
-        if number > 0:
-            pos+=1
-        elif number == 0:
-            zero+=1
-        else:
-            negs+=1
-    negratio = negs/len(arr)
-    posratio = pos/len(arr)
-    zeroratio = zero/len(arr)
-    print("{:.6f}".format(posratio))
-    print("{:.6f}".format(negratio))
-    print("{:.6f}".format(zeroratio))
+  length = len(arr)  
+  pos_count = sum(1 for x in arr if x > 0)
+  neg_count = sum(1 for x in arr if x < 0)
+  zero_count = sum(1 for x in arr if x == 0)
+
+  pos_ratio = pos_count / length
+  neg_ratio = neg_count / length
+  zero_ratio = zero_count / length
+
+  print(f"{pos_ratio:.6f}")
+  print(f"{neg_ratio:.6f}")
+  print(f"{zero_ratio:.6f}")
         
 
 if __name__ == '__main__':
-    n = int(input().strip())
+  n = int(input().strip())
 
-    arr = list(map(int, input().rstrip().split()))
+  arr = list(map(int, input().rstrip().split()))
 
-    plusMinus(arr)
+  plusMinus(arr)
