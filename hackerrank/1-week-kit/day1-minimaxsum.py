2feaefee4bf640dfa14c914c3de0417b685d75be
mendesmcmg <claraplanejamento@gmail.com>
2024-10-04 12:12:41 -0300

    👑 day 1 - ex. 2

---
+++ hackerrank/1-week-kit/day1-minimaxsum.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    mini = min(arr)
+    maxi = max(arr)
+    
+    minisum = sum(x for x in arr if x != maxi)
+    maxisum = sum(x for x in arr if x != mini)
+    print(f"{minisum} {maxisum}")
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
