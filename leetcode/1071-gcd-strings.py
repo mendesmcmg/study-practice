262bc2f627f1ab22e5c6b8450ae0c6ca61cbf68d
mendesmcmg <claraplanejamento@gmail.com>
2024-10-04 22:43:42 -0300

    🌃  day 1 - ex. 2 (fixed)

--- leetcode/1071-gcd-strings.py
+++ leetcode/1071-gcd-strings.py
@@ -2,10 +2,17 @@ class Solution:
   def gcdOfStrings(self, str1: str, str2: str) -> str:
     len1 = len(str1)
     len2 = len(str2)
-    base = str2
-    for i in range(len2, -1, -1):
+    minlen = min(len1,len2)
+    maxlen = max(len1,len2)
+    base = str2 if len1 > len2 else str1
+    minim = base
+    other = str1 if len1 > len2 else str2
+    for i in range(minlen, -1, -1):
       lenb = len(base)
-      if base == "" or len1 % lenb == 0 and base * int(len1/lenb) == str1:
+      if base == "":
+        return base
+      elif maxlen % lenb == 0 and minlen % lenb == 0 and base * int(maxlen/lenb) == other and base * int(minlen/lenb) == minim:
         return base
       else:
         base = base[:-1]
+
