47bf42e5d9bf804bcfbb0150384d964d4a47791e
mendesmcmg <claraplanejamento@gmail.com>
2024-10-04 21:44:49 -0300

    🌊 day 1 - ex. 1 (refactored)

--- leetcode/1768-merge-alternately.py
+++ leetcode/1768-merge-alternately.py
@@ -1,22 +1,16 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        len1 = len(word1)
-        len2 = len(word2)
-        rangemax = max(len1, len2)
-        rangemin1 = 0 if len1 > len2 else 1
-        out = ""
-        for i in range(rangemax):
-            if rangemin1 == 1 and i < len1:
-                out+= word1[i]
-                out+=word2[i]
-                continue
-            if rangemin1 == 0 and i < len2:
-                out+= word1[i]
-                out+=word2[i]
-                continue
-            elif rangemin1 == 1:
-                out+=word2[i]
-            elif rangemin1 == 0:
-                out+= word1[i]
-        return out
-        
+        merged = []
+        len1, len2 = len(word1), len(word2)
+        min_len = min(len1, len2)
+
+        for i in range(min_len):
+            merged.append(word1[i])
+            merged.append(word2[i])
+
+        if len1 > len2:
+            merged.append(word1[min_len:])
+        else:
+            merged.append(word2[min_len:])
+
+        return ''.join(merged)
