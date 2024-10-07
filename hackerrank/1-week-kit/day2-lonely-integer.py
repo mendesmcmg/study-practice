e278c1e9b4baba3feed019ef7cb5d0ccd65c6dcd
mendesmcmg <claraplanejamento@gmail.com>
2024-10-07 11:00:13 -0300

    ☯ day 2 - ex. 1

---
+++ hackerrank/1-week-kit/day2-lonely-integer.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    hist = {}
+    for item in a:
+        if item in hist:
+            hist[item] += 1
+        else:
+            hist[item] = 1
+    for el in hist:
+        if hist[el] == 1:
+            return el
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
